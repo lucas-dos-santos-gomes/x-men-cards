cfe4705414293f25b6601e7e3c2341b73397c1f9
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-07-11 12:29:54 -0300

    Deixando os personagens em ordem alfabética

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,11 +7,11 @@ const descriptions = {
   ciclope: "Ele tem o poder de disparar rajadas ópticas por um acidente que aconteceu quando ele era criança, mas não consegue controlá-los",
   jeangrey: "Os poderes de Jean eram restritos a simples telecinese, e até mesmo levantar seu próprio peso rapidamente cansava ela.",
   lincenegra: "Ela tem o poder de atravessar matéria sólida,campos de energia,rajadas ópticas, continuando intacta,e também tem o poder de andar no ar.",
+  magneto: "Ele tem o poder de magnetismo, ou seja, controlar objetos de metal e criar campos magnéticos.",
+  noturno: "Ele é um mutante que nasceu com cauda, orelhas pontudas, mãos com três dedos e pés com dois dedos.",
   tempestade: "Tem o poder de controlar as forças da natureza, faz o clima variar entre seus extremos. Ela pode produzir raios, chuvas, gelos, ventos, neblina, entre outros efeitos.",
   vampira: "Ela tem o poder de absorver a vitalidade e memória das pessoas que toca ou no caso dos mutantes os poderes e a memória, mas só temporariamente.",
   wolverine: "Wolverine é um dos mutantes mais importante e reconhecido da história dos X-Men.",
-  noturno: "Ele é um mutante que nasceu com cauda, orelhas pontudas, mãos com três dedos e pés com dois dedos.",
-  magneto: "Ele tem o poder de magnetismo, ou seja, controlar objetos de metal e criar campos magnéticos."
 }
 
 function scrollTop() {
